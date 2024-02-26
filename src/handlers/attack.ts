c2413d551fb23a99c02ee269cb15ede7c93bbbe1
Uladimir Hrydzin <hrydzin@gmail.com>
2024-02-26 15:16:10 +0400

    featt: add mesaage miss around killed ships

--- src/handlers/attack.ts
+++ src/handlers/attack.ts
@@ -1,10 +1,10 @@
 import { WebSocket } from 'ws';
-
 import { wss } from "../constants/constants";
 import { AttackMessage } from "../types/types";
 import { CustomWebSocket } from "../ws/customwebsocket";
 import { rooms } from '../constants/constants';
 import { sendTurnInfo } from '..';
+
 
 export function attack(ws: CustomWebSocket, data: AttackMessage): void {
     const { gameId, x, y, indexPlayer } = JSON.parse(data.data);
@@ -34,12 +34,10 @@ export function attack(ws: CustomWebSocket, data: AttackMessage): void {
     const position = { x, y };
     let status = "miss";
 
-
-
     opponentShips.forEach(ship => {
         const isHorizontal = ship.direction;
         const posCheck = isHorizontal ? x === ship.position.x && y >= ship.position.y && y < ship.position.y + ship.length :
-                                        y === ship.position.y && x >= ship.position.x && x < ship.position.x + ship.length;
+            y === ship.position.y && x >= ship.position.x && x < ship.position.x + ship.length;
         if (posCheck) {
             const index = isHorizontal ? y - ship.position.y : x - ship.position.x;
             ship.hits[index] = true;
@@ -51,35 +49,80 @@ export function attack(ws: CustomWebSocket, data: AttackMessage): void {
                         type: "attack",
                         data: JSON.stringify({
                             position: isHorizontal ? { x: ship.position.x, y: ship.position.y + i } : { x: ship.position.x + i, y: ship.position.y },
-                            currentPlayer: indexPlayer, status: "killed" }),
-                            id: 0 };
+                            currentPlayer: indexPlayer, status: "killed"
+                        }),
+                        id: 0
+                    };
 
                     wss.clients.forEach(client => {
+                        if (client.readyState === WebSocket.OPEN) {
+                            client.send(JSON.stringify(feedbackMessage));
+                        }
+                    });
+                }
+
+                for (let i = ship.position.x - 1; i <= ship.position.x + (isHorizontal ? 1 : ship.length); i++) {
+                    for (let j = ship.position.y - 1; j <= ship.position.y + (isHorizontal ? ship.length : 1); j++) {
+                        if (
+                            i >= 0 && i < 10 && j >= 0 && j < 10 &&
+                            !(i === x && j === y) &&
+                            opponentShips.every(s => {
+                                const isHorizontal = s.direction;
+                                return !(
+                                    isHorizontal ? i === s.position.x && j >= s.position.y && j < s.position.y + s.length :
+                                        j === s.position.y && i >= s.position.x && i < s.position.x + s.length
+                                );
+                            })
+                        ) {
+                            const feedbackMessage = {
+                                type: "attack",
+                                data: JSON.stringify({
+                                    position: { x: i, y: j },
+                                    currentPlayer: indexPlayer,
+                                    status: "miss"
+                                }),
+                                id: 0
+                            };
+
+                            wss.clients.forEach(client => {
                                 if (client.readyState === WebSocket.OPEN) {
                                     client.send(JSON.stringify(feedbackMessage));
                                 }
                             });
+                        }
+                    }
                 }
-            } else status = "shot";
+            } else {
+                status = "shot";
+            }
+        } else {
+            const isHorizontal = ship.direction;
+            const aroundShip = isHorizontal ?
+                (x === ship.position.x - 1 && y >= ship.position.y - 1 && y < ship.position.y + ship.length + 1) :
+                (y === ship.position.y - 1 && x >= ship.position.x - 1 && x < ship.position.x + ship.length + 1);
+            if (aroundShip) {
+                status = "miss";
+            }
         }
     });
 
+    if (status !== "killed") {
+        const feedbackMessage = {
+            type: "attack",
+            data: JSON.stringify({
+                position: position,
+                currentPlayer: indexPlayer,
+                status: status
+            }),
+            id: 0
+        };
 
-    const feedbackMessage = {
-        type: "attack",
-        data: JSON.stringify({
-            position: position,
-            currentPlayer: indexPlayer,
-            status: status
-        }),
-        id: 0
-    };
-
-    wss.clients.forEach(client => {
-                if (client.readyState === WebSocket.OPEN) {
-                    client.send(JSON.stringify(feedbackMessage));
-                }
-            });
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(feedbackMessage));
+            }
+        });
+    }
 
     playerRoom.nextPlayerIndex = status === "miss" ? opponentId : indexPlayer;
     sendTurnInfo(ws);
